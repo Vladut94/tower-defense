8145b6929940dbc8c1f9ffddd8522c764a668c53
Vladut02 <vladut16.mihai94@gmail.com>
2023-01-11 17:36:52 +0200

    create placements tiles

--- js/index.js
+++ js/index.js
@@ -15,46 +15,6 @@ image.onload = () => {
 image.src = 'assets/gameMap.png';
 
 //enemy creation
-class Enemy {
-    constructor({ position = { x: 0, y: 0} }) {
-        this.position = position
-        this.width = 100
-        this.height = 100
-        this.waypointIndex = 0
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-    }
-
-    draw() {
-        context.fillStyle = 'red';
-        context.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-
-    update() {
-        this.draw()
-
-        const waypoint = waypoints[this.waypointIndex]
-        const yDistance = waypoint.y - this.center.y
-        const xDistance = waypoint.x - this.center.x
-        const angle = Math.atan2(yDistance, xDistance)
-        this.position.x += Math.cos(angle)
-        this.position.y += Math.sin(angle)
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-        
-        if ( Math.round(this.center.x) === Math.round(waypoint.x) && 
-             Math.round(this.center.y) === Math.round(waypoint.y) &&
-             this.waypointIndex < waypoints.length - 1
-            ) {
-            this.waypointIndex++
-        }
-    }
-}
-
 const enemies = []
 for (let i = 1; i < 10; i++) {
     const xOffset = i * 150
@@ -63,6 +23,34 @@ for (let i = 1; i < 10; i++) {
     }))
 }
 
+
+
+//placement creation
+const placementTilesData2D = []
+for (let i = 0; i < placementTilesData.length; i += 20) {
+    placementTilesData2D.push(placementTilesData.slice(i, i + 20))
+}
+
+
+
+const placementTiles = [];
+
+placementTilesData2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol === 14) {
+            //add building placement tile here
+            placementTiles.push(
+                new PlacementTile({
+                    position: {
+                        x: x * 64,
+                        y: y * 64
+                    }
+                })
+            )
+        }
+    })
+})
+
 function animate() {
     requestAnimationFrame(animate)
 
@@ -70,4 +58,19 @@ function animate() {
     enemies.forEach(enemy => {
         enemy.update()
     })
+
+    placementTiles.forEach(tile => {
+        tile.update(mouse)
+    })
 }
+
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+})
